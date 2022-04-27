d992289ee349314b227fb5a79706b80d81bde109
gludek <gludekpl@gmail.com>
2022-04-27 22:46:42 +0200

    Quality Checks

--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -1,6 +1,6 @@
 class Kudo < ApplicationRecord
-  belongs_to :giver, class_name:'Employee'
-  belongs_to :receiver, class_name:'Employee'
+  belongs_to :giver, class_name: 'Employee'
+  belongs_to :receiver, class_name: 'Employee'
 
-  validates :title,:context,presence: true
+  validates :title, :context, presence: true
 end
