536c4141900514dc5d60f674ac38fd4ccfc9acb8
Gludek <gludekpl@gmail.com>
2022-04-30 18:59:48 +0200

    Test employee authentication system

---
+++ spec/factories/employees.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+	factory :employee do
+		sequence(:email){ |i| "user#{i}@test.com"}
+		password{ 'password'};
+	end
+  end
+  
---
+++ spec/system/employee_authentication_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Employee authentication actions' do
+	let(:employee) { build(:employee) }
+
+	it 'allows to create account, log out and log in' do
+		visit root_path
+
+		click_link 'Sign up'
+		fill_in "Email",	with: employee.email
+		fill_in "Password",	with: employee.password
+		fill_in "Password confirmation",	with: employee.password
+		click_button 'Sign up'
+
+		expect(page).to have_content 'Welcome! You have signed up successfully.'
+
+		click_link 'Sign out'
+
+		expect(page).to have_content 'You need to sign in or sign up before continuing.'
+
+		visit root_path
+		expect(page).to have_content 'Log in'
+		fill_in "Email",	with: employee.email
+		fill_in "Password",	with: employee.password
+		click_button 'Log in'
+		expect(page).to have_content 'Signed in successfully.'
+		click_link 'Sign out'
+
+		expect(page).to have_content 'You need to sign in or sign up before continuing.'
+	end
+end
