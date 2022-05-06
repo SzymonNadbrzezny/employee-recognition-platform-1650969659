f210fbe1ee19dd22c178032222cffc13fca3a790
gludek <gludekpl@gmail.com>
2022-05-06 15:35:53 +0200

    Setup of views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,19 @@
 
 Rails.application.routes.draw do
   resources :kudos
-  devise_for :employees
+  devise_for :admins,path: 'admins',path_names: {
+    sign_in: '/', sign_out: 'logout',
+    password: 'secret', confirmation: 'verification',
+    registration: 'register', edit: 'edit/profile'
+  }, controllers: {
+    sessions: 'admins/sessions'
+  }
+  devise_for :employees, path: 'employees', controllers: {
+    sessions: 'employees/sessions'
+  }
+  namespace :admins do
+    get "/dashboard", to: "pages#dashboard"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root to: "kudos#index"
 end
