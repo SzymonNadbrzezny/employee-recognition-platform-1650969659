b4a1d37735063d0ff5c42aef0a66c6d55a3c1532
gludek <gludekpl@gmail.com>
2022-05-06 15:36:32 +0200

    system specs for admin model

---
+++ spec/system/admin_authentication_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin authentication actions' do
+  let!(:admin) { create(:admin) }
+
+  it 'allow to log in and log out' do
+    visit admins_root_path
+
+    expect(page).to have_content 'Log in'
+    fill_in 'Email',	with: admin.email
+    fill_in 'Password',	with: admin.password
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully.'
+    click_link 'Sign out'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
