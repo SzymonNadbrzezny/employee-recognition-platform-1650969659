670f1b7b8819e6e902c8a9c860f73c8ea478ef83
gludek <gludekpl@gmail.com>
2022-05-08 00:43:14 +0200

    Kudo RD actions for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :kudos
-  devise_for :admins,path: 'admins',path_names: {
-    sign_in: '/', sign_out: 'logout',
-    password: 'secret', confirmation: 'verification',
-    registration: 'register', edit: 'edit/profile'
-  }, controllers: {
-    sessions: 'admins/sessions'
-  }
-  devise_for :employees, path: 'employees', controllers: {
-    sessions: 'employees/sessions'
-  }
   namespace :admins do
-    get "/dashboard", to: "pages#dashboard"
+    resources :kudos
+  end
+  resources :kudos
+  devise_for :admins, path: "admins", controllers: {
+                        sessions: "admins/sessions",
+                      }
+  devise_for :employees, path: "employees"
+  namespace :admins do
+    resources :kudos
+    root to: "pages#dashboard"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "kudos#index"
