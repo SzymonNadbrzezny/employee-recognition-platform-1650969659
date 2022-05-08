b12b9a8bce5e10479ce8d3673f2d42f9f700ba65
gludek <gludekpl@gmail.com>
2022-05-08 06:07:39 +0200

    feat: :sparkles: Implementation of new field into website
    Displaying avaible kudos as well as new checks to meet requirements

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,15 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def number_of_avaible_kudos_greater_than_zero?
+    errors.add(:number_of_avaible_kudos, "can't be below 0") if number_of_avaible_kudos.negative?
+    number_of_avaible_kudos.positive?
+  end
+
+  def decrease_kudos(employee)
+    avaible_kudos = (employee.number_of_avaible_kudos - 1)
+    avaible_kudos = 0 if avaible_kudos.negative?
+    Employee.update(employee.id, number_of_avaible_kudos: avaible_kudos)
+  end
 end
