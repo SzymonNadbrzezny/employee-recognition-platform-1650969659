10f4198f155bd51b14f87f728d138d41807c1127
gludek <gludekpl@gmail.com>
2022-05-16 15:12:23 +0200

    better association handling

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -22,8 +22,8 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :kudos, foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :receiver
-  has_many :kudos, foreign_key: 'giver_id', dependent: :destroy, inverse_of: :giver
+  has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :receiver
+  has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', dependent: :destroy, inverse_of: :giver
 
   def decrease_kudos(employee)
     avaible_kudos = (employee.number_of_avaible_kudos - 1)
