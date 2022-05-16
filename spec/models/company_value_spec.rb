6aa7f4d902ac69bc23d3457b5f7eea8f9a9279d7
gludek <gludekpl@gmail.com>
2022-05-16 20:14:54 +0200

    fixed issues in feature

--- spec/models/company_value_spec.rb
+++ spec/models/company_value_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe CompanyValue, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:title) }
-    it { should validate_uniqueness_of(:title).case_insensitive }
+    it { is_expected.to validate_uniqueness_of(:title).case_insensitive }
   end
 end
