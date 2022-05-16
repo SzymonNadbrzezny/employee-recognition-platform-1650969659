97a656d3d01c99755c081b3c6543e6150a057c57
gludek <gludekpl@gmail.com>
2022-05-16 19:42:46 +0200

    Rubocop styling

--- spec/models/company_value_spec.rb
+++ spec/models/company_value_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CompanyValue, type: :model do
   let!(:company_value2) { create(:company_value, title: 'test') }
 
   describe 'validations' do
-    it { should validate_presence_of(:title) }
-    it { should validate_uniqueness_of(:title).case_insensitive }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_uniqueness_of(:title).case_insensitive }
   end
 end
