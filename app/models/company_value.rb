2cb124c8d7dea02f4bc17bda3de7974430952861
gludek <gludekpl@gmail.com>
2022-05-20 16:57:57 +0200

    feat: :sparkles: Creating association in models

--- app/models/company_value.rb
+++ app/models/company_value.rb
@@ -12,5 +12,7 @@
 #  unique_titles  (title) UNIQUE
 #
 class CompanyValue < ApplicationRecord
+  has_many :kudo, dependent: :destroy, inverse_of: :company_value
+
   validates :title, presence: true, uniqueness: { case_sensitive: false }
 end
