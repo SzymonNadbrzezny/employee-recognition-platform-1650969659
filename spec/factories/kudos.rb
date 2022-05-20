6d6c5ac0bafd065bab767740d434f4c76280d01c
gludek <gludekpl@gmail.com>
2022-05-20 16:59:46 +0200

    feat: :sparkles: Updating specs to check new association

--- spec/factories/kudos.rb
+++ spec/factories/kudos.rb
@@ -2,21 +2,24 @@
 #
 # Table name: kudos
 #
-#  id          :bigint           not null, primary key
-#  content     :text             not null
-#  title       :string           not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  giver_id    :bigint           not null
-#  receiver_id :bigint           not null
+#  id               :bigint           not null, primary key
+#  content          :text             not null
+#  title            :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  company_value_id :bigint           not null
+#  giver_id         :bigint           not null
+#  receiver_id      :bigint           not null
 #
 # Indexes
 #
-#  index_kudos_on_giver_id     (giver_id)
-#  index_kudos_on_receiver_id  (receiver_id)
+#  index_kudos_on_company_value_id  (company_value_id)
+#  index_kudos_on_giver_id          (giver_id)
+#  index_kudos_on_receiver_id       (receiver_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (company_value_id => company_values.id)
 #  fk_rails_...  (giver_id => employees.id)
 #  fk_rails_...  (receiver_id => employees.id)
 #
@@ -25,6 +28,7 @@ FactoryBot.define do
     title { 'Kudo-test' }
     content { 'Don\'t mind me, just testing!' }
 
+    company_value factory: :company_value
     receiver factory: :employee
     giver factory: :employee
   end
--- spec/system/kudo_handling_spec.rb
+++ spec/system/kudo_handling_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Kudo handling' do
   let(:employee) { create(:employee, number_of_avaible_kudos: 1) }
   let!(:receiver) { create(:employee) }
+  let!(:company_value) { create(:company_value) }
+  let!(:company_value2) { create(:company_value) }
 
   before do
     sign_in employee
@@ -13,18 +15,21 @@ RSpec.describe 'Kudo handling' do
     click_link 'New Kudo'
     fill_in 'Title',	with: 'Kudo title'
     fill_in 'Content',	with: 'kudo content'
+    select company_value2.title, from: 'Company value'
     click_button 'Create Kudo'
     expect(page).to have_content('Avaible Kudos0')
     expect(page).to have_content 'Kudo was successfully created.'
     expect(page).to have_content 'Kudo title'
+    expect(page).to have_content company_value2.title
 
     click_link 'Edit'
 
     fill_in 'Title',	with: 'Kudo edited title'
+    select company_value.title, from: 'Company value'
     click_button 'Update Kudo'
 
     expect(page).to have_content 'Kudo edited title'
-
+    expect(page).to have_content company_value.title
     click_link 'Destroy'
     expect(page).to have_content 'Kudo was successfully destroyed.'
   end
