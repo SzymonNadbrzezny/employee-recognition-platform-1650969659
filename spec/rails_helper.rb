ee358d4b6fe7b70a110bc22cb2fa641e77e5e1e4
gludek <gludekpl@gmail.com>
2022-05-20 15:50:19 +0200

    Fixing support spec location

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,8 +22,7 @@ require 'rspec/rails'
 # of increasing the boot-up time by auto-requiring all files in the support
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
-#
- Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f}
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f}
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
@@ -65,11 +64,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.before(:each, type: :system) do
     driven_by(:rack_test)
-  end
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
   end
 end
