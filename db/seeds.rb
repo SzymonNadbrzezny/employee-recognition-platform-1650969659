0e890df4d27671d6b80aaf9005d2825d025035c9
gludek <gludekpl@gmail.com>
2022-05-24 00:33:40 +0200

    feat: :sparkles: Seeds for new model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'faker'
 1.upto(5) do |i|
   Employee.where(email: "employee#{i}@test.com").first_or_create!(password: 'password')
 end
@@ -13,12 +13,17 @@ puts 'Admin account creation'
 Admin.where(email: 'admin@test.com').first_or_create!(password: 'admintest')
 
 puts 'Company values creation'
-CompanyValue.where(title: 'Honesty').first_or_create!
-CompanyValue.where(title: 'Ownership').first_or_create!
-CompanyValue.where(title: 'Accountability').first_or_create!
-CompanyValue.where(title: 'Passion').first_or_create!
+%w[Honesty Ownership Accountability Passion].each do |company_value_title|
+  CompanyValue.where(title: company_value_title).first_or_create!
+end
 2.upto(5) do |i|
-  Kudo.where(title: " Seeds for employee#{i}@test.com").first_or_create!(content: 'smh',
+  Reward.where(title: "Reward nr. #{i}").first_or_create!(description: Faker::Creature::Animal,
+                                                          price: Faker::Number.decimal(
+                                                            l_digits: 3, r_digits: 3
+                                                          ))
+end
+2.upto(5) do |i|
+  Kudo.where(title: " Seeds for employee#{i}@test.com").first_or_create!(content: Faker::Creature::Animal,
                                                                          giver: Employee.all.sample,
                                                                          receiver: Employee.find_by(email: "employee#{i}@test.com"),
                                                                          company_value: CompanyValue.find_by(title: 'Honesty'))
