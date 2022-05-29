5ddd77f7157bc40070c16239679fd123ee5e5a8e
gludek <gludekpl@gmail.com>
2022-05-29 18:26:52 +0200

    fix: Fixed code error due to rubocop bug

--- app/controllers/admins/employees_controller.rb
+++ app/controllers/admins/employees_controller.rb
@@ -24,12 +24,12 @@ module Admins
     # PATCH/PUT /employees/1
     def update
       if employee_params[:password].empty?
-        if employee.update(employee_params)
+        if employee.update_without_password(employee_params.except(:password))
           redirect_to admins_employees_path, notice: 'Employee was successfully updated.'
         else
           render :edit, locals: { employee: employee }
         end
-      elsif employee.update_without_password(employee_params.except(:password))
+      elsif employee.update(employee_params)
         redirect_to admins_employees_path, notice: 'Employee was successfully updated.'
       else
         render :edit, locals: { employee: employee }
