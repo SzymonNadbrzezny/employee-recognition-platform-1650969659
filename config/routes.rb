f3c642511ce6d1dcb59ed0cd26bdfdab8ffbb375
gludek <gludekpl@gmail.com>
2022-07-22 01:17:44 +0200

    style: :art: Rubucop styling and removed unnecessary path

--- config/routes.rb
+++ config/routes.rb
@@ -10,27 +10,22 @@ Rails.application.routes.draw do
   }
   namespace :admins do
     resources :kudos
-    resources :employees, except: :create do
-      member do
-        resources :orders, only: %i[index show] , as: 'employee_orders'
-      end
-    end
+    resources :employees, except: :create
     resources :rewards
     resources :company_values
-    
+
     root to: 'pages#dashboard'
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  scope module: "employees" do
+  scope module: 'employees' do
     resources :kudos
-  resources :rewards, only: %i[index show]
-  resources :orders, only: %i[new create] 
-  resources :employees do
-    member do
-      resources :orders, only: %i[index show]
-      get "orders", to: "order#index"
+    resources :rewards, only: %i[index show]
+    resources :orders, only: %i[new create]
+    resources :employees do
+      member do
+        resources :orders, only: %i[index show]
+        get 'orders', to: 'order#index'
+      end
     end
+    root to: 'kudos#index'
   end
-  root to: 'kudos#index'
-end
 end
