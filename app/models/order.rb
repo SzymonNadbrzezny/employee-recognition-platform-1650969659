3ec3a65226db3fb5919ca538c0d725864c2bd1d8
gludek <gludekpl@gmail.com>
2022-07-24 01:52:10 +0200

    feat: :sparkles: Introduced order status as enum and scopes for easier handling

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :buyer, class_name: 'Employee'
   serialize :reward, JSON
+  enum status: { awaiting_delivery: 'awaiting_delivery', delivered: 'delivered' }
+  scope :ordered_by_status, ->(ordering_method = 'asc') { order(status: ordering_method) }
+  scope :undelivered, -> { where(status: :awaiting_delivery) }
 end
