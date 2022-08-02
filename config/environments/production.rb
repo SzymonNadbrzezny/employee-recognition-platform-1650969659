de2b277c2c9f035ce09f3def1162d29d1fe49516
gludek <gludekpl@gmail.com>
2022-08-02 23:21:19 +0200

    style: :art: Improved formating of code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,12 +91,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.smtp_settings = {
     :address   => 'smtp.sendgrid.net',
-     :port      => 587,
-     :domain    => 'gludek-app.heroku.com',
-     :user_name => 'apikey',
-     :password  => Rails.application.credentials.sendgrid[:api_key],
-     :authentication => 'plain',
-     :enable_starttls_auto => true }
+    :port      => 587,
+    :domain    => 'gludek-app.heroku.com',
+    :user_name => 'apikey',
+    :password  => Rails.application.credentials.sendgrid[:api_key],
+    :authentication => 'plain',
+    :enable_starttls_auto => true 
+  }
   config.action_mailer.delivery_method = :smtp
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
