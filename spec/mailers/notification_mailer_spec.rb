0d5a258759635c878e6a96ef65c418f2a687536b
gludek <gludekpl@gmail.com>
2022-08-02 23:17:18 +0200

    test: :white_check_mark: Created tests for notification mailer

---
+++ spec/mailers/notification_mailer_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Notifications', type: :mailer do
+  
+  describe '- delivery ' do
+    let!(:employee) { create(:employee, points: 10) }
+    let!(:reward) { create(:reward, price: 2) }
+    let!(:order) { create(:order, buyer: employee, reward: reward.to_json) }
+    let(:mail) { NotificationsMailer.with(employee: employee, order: order).notify_of_delivery}
+
+    it "renders the headers" do
+      expect(mail.subject).to have_content "Confirmation of order delivery", exact: true
+      expect(mail.from).to have_content "gludekpl@gmail.com"
+      expect(mail.to).to have_content employee.email
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to have_content "Order nr. #{order.id}"
+    end
+
+  end
+end
