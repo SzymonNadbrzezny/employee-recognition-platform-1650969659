4de47c1a75d52c920dbc4916484a4d38c8e9c3c3
gludek <gludekpl@gmail.com>
2022-08-02 23:58:22 +0200

    feat: :sparkles: Allow devise to be used in background job

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,4 +6,9 @@ class Employee < ApplicationRecord
   has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :receiver
   has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', dependent: :destroy, inverse_of: :giver
   has_many :orders, class_name: 'Order', foreign_key: 'buyer_id', dependent: :destroy, inverse_of: :buyer
+
+  # Allows to use devise in background job
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
