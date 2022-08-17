fb4b5e04f699e429e6130e23cde5956c1483a17e
gludek <gludekpl@gmail.com>
2022-08-17 15:02:39 +0200

    feat: :sparkles: Added attachment photo for rewards model

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,6 +2,7 @@ class Reward < ApplicationRecord
   paginates_per 3
   has_many :categories_rewards, dependent: :delete_all
   has_many :categories, through: :categories_rewards
+  has_one_attached :picture
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 1 }
   validates :categories, presence: true
