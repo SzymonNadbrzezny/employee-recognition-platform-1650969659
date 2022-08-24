ba9b7de9c5c4b20881dd3dcd4473ea2b484ddc6e
gludek <gludekpl@gmail.com>
2022-08-24 14:35:26 +0200

    feat: :sparkles: Added association to reward in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :buyer, class_name: 'Employee'
-  serialize :reward, JSON
+  belongs_to :reward, class_name: 'Reward'
   enum status: { awaiting_delivery: 0, delivered: 1 }
   scope :ordered_by_status, ->(ordering_method = 'asc') { order(status: ordering_method) }
 end
