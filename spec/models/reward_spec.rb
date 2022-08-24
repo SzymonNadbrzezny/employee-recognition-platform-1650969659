7870a44cbb25222d94568b29f7d0c1fca62588c5
gludek <gludekpl@gmail.com>
2022-08-24 02:09:04 +0200

    test: :white_check_mark: Added tests for validating content type

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Reward, type: :model do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(1) }
+    it { is_expected.to validate_content_type_of(:picture).allowing('image/png', 'image/jpg') }
   end
 end
