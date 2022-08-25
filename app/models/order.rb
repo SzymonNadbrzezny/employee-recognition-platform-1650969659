e629b60671cf3ccf4338f17dd91f3d86b9676870
gludek <gludekpl@gmail.com>
2022-08-25 16:29:35 +0200

    style: :art: Implemented suggested changes in formatting

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :buyer, class_name: 'Employee'
-  belongs_to :reward, class_name: 'Reward'
+  belongs_to :reward
   enum status: { awaiting_delivery: 0, delivered: 1 }
   scope :ordered_by_status, ->(ordering_method = 'asc') { order(status: ordering_method) }
 end
