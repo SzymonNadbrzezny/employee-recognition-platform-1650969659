ea771adc1f09c86ea201ac064173aecb47c1c385
gludek <gludekpl@gmail.com>
2022-09-21 09:25:23 +0200

    feat: :sparkles: Add first and last name to employee model

---
+++ db/migrate/20220921062224_add_first_and_last_name_to_employee.rb
@@ -0,0 +1,6 @@
+class AddFirstAndLastNameToEmployee < ActiveRecord::Migration[6.1]
+  def change
+    add_column :employees, :first_name, :string, null: false
+    add_column :employees, :last_name, :string, null: false
+  end
+end
