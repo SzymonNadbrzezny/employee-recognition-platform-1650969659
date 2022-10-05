28cc854bf957396ab7c53601092e1f297327e5f5
gludek <gludekpl@gmail.com>
2022-10-05 02:34:34 +0200

    test: :white_check_mark: Updated tests to check newly added support for post delivery method

--- spec/system/employees/order_handling_spec.rb
+++ spec/system/employees/order_handling_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe 'Orders handling allows' do
   let!(:employee) { create(:employee, points: 10) }
@@ -18,8 +19,6 @@ RSpec.describe 'Orders handling allows' do
     expect(page).to have_content reward.price
     expect(page).to have_content 'Buy'
 
-    visit rewards_path
-
     click_link 'Buy'
     expect(page).to have_content 'New Order'
     expect(page).to have_content reward.title
@@ -27,6 +26,37 @@ RSpec.describe 'Orders handling allows' do
     expect(page).to have_button 'Create Order'
     click_button 'Create Order'
     expect(page).to have_content("Current points #{10 - reward.price}")
+  end
+
+  it 'Employee to create orders for rewards with delivery by post' do
+    reward.post!
+    street = Faker::Address.street_address
+    city = Faker::Address.city
+    postal_code = Faker::Address.zip_code
+
+    visit rewards_path
+
+    expect(page).to have_content 'Rewards'
+    expect(page).to have_content reward.title
+    expect(page).to have_content reward.price
+    expect(page).to have_content 'Buy'
+
+    click_link 'Buy'
+    expect(page).to have_content 'New Order'
+    expect(page).to have_content reward.title
+    expect(page).to have_content reward.price
+    expect(page).to have_content 'Street'
+    expect(page).to have_content 'Postal code'
+    expect(page).to have_content 'City'
+    expect(page).to have_button 'Create Order'
+
+    fill_in 'order_form_city', with: city
+    fill_in 'order_form_street', with: street
+    fill_in 'order_form_postal_code', with: postal_code
+
+    click_button 'Create Order'
+    expect(page).to have_content("Current points #{employee.points - reward.price}")
+    expect(page).to have_content('Order was successfully created.')
   end
 
   it 'Employee to list their Orders and see price of reward at time of purchase' do
