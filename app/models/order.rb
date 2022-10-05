79f6400594f63557ebee9093e09eda46cbc1c825
gludek <gludekpl@gmail.com>
2022-10-05 02:28:17 +0200

    feat: :sparkles: Added serialized address to Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   belongs_to :reward
   enum status: { awaiting_delivery: 0, delivered: 1 }
   scope :ordered_by_status, ->(ordering_method = 'asc') { order(status: ordering_method) }
+  serialize :address_snapshot
 end
